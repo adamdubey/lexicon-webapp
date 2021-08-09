a2b332c822bcb8da2c7134b4ac1824ad52bddb6a
Adam Dubey <adubey0311@gmail.com>
2021-08-09 16:30:30 -0400

    [AD][08M0921] Complete initial app development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Switch, withStyles } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Definitions from "./components/Definitions/Definitions";
+import Header from "./components/Header/Header";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [language, setlanguage] = useState("en");
+  const [LightTheme, setLightTheme] = useState(false);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${language}/${word}`
+      );
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    dictionaryApi();
+    // eslint-disable-next-line
+  }, [word, language]);
+
+  const PurpleSwitch = withStyles({
+    switchBase: {
+      color: grey[50],
+      "&$checked": {
+        color: grey[900],
+      },
+      "&$checked + $track": {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        backgroundColor: LightTheme ? "#fff" : "#282c34",
+        color: LightTheme ? "black" : "white",
+        transition: "all 0.5s linear",
+      }}
+    >
+      <Container
+        maxWidth="md"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
         >
-          Learn React
-        </a>
-      </header>
+          <span>{LightTheme ? "Dark" : "Light"} Mode</span>
+          <PurpleSwitch
+            checked={LightTheme}
+            onChange={() => setLightTheme(!LightTheme)}
+          />
+        </div>
+        <Header
+          setWord={setWord}
+          language={language}
+          setLanguage={setlanguage}
+          word={word}
+          setMeanings={setMeanings}
+          LightTheme={LightTheme}
+        />
+        {meanings && (
+          <Definitions
+            meanings={meanings}
+            word={word}
+            LightTheme={LightTheme}
+            language={language}
+          />
+        )}
+      </Container>
     </div>
   );
 }
